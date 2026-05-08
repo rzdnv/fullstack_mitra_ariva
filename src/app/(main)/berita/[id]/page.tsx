5b080295847af4030fd263b50db1e43002bd1dd4
Affreza Dinova <affrezad@gmail.com>
2026-05-08 15:27:04 +0700

    fix: fix import path case sensitivity

--- src/app/(main)/berita/[id]/page.tsx
+++ src/app/(main)/berita/[id]/page.tsx
@@ -1,4 +1,4 @@
-import DetailBerita from "@/components/views/main/detailberita/DetailBerita";
+import DetailBerita from "@/components/views/main/detail-berita/DetailBerita";
 import { Metadata } from "next";
 
 export const metadata: Metadata = { title: "Detail Berita" };
