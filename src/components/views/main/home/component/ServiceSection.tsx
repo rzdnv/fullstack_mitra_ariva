14b3325b9754d614597bbc33cf27e9b10195c672
Affreza Dinova <affrezad@gmail.com>
2026-05-23 10:04:09 +0700

    fix: service section

--- src/components/views/main/home/component/ServiceSection.tsx
+++ src/components/views/main/home/component/ServiceSection.tsx
@@ -29,7 +29,6 @@ const ServiceSection = () => {
                   key={layanan.id}
                   foto={`${layanan.foto}`}
                   namaLayanan={layanan.namaLayanan}
-                  deskripsi={layanan.deskripsi}
                 />
               ))}
         </div>
