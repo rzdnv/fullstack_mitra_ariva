d2f77409e7b0d3242dae18e88c6e5ff22c3ff04f
Affreza Dinova <affrezad@gmail.com>
2026-06-03 14:57:20 +0700

    fix: on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,38 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-// export async function middleware(req: NextRequest) {
-//   const token = await getToken({
-//     req,
-//     secret: process.env.AUTH_SECRET,
-//   });
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  });
 
-//   const isLoggedIn = !!token;
-//   const role = token?.role as string | undefined;
-//   const { pathname } = req.nextUrl;
+  const isLoggedIn = !!token;
+  const role = token?.role as string | undefined;
+  const { pathname } = req.nextUrl;
 
-//   const isLoginPage = pathname === "/login";
-//   const isAdminRoute = pathname.startsWith("/admin");
-//   const isUsersRoute = pathname.startsWith("/admin/users");
+  const isLoginPage = pathname === "/login";
+  const isAdminRoute = pathname.startsWith("/admin");
+  const isUsersRoute = pathname.startsWith("/admin/users");
 
-//   // Sudah login tapi akses halaman login → redirect ke admin
-//   if (isLoggedIn && isLoginPage) {
-//     return NextResponse.redirect(new URL("/admin", req.url));
-//   }
+  // Sudah login tapi akses halaman login → redirect ke admin
+  if (isLoggedIn && isLoginPage) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
 
-//   // Belum login tapi akses halaman admin → redirect ke login
-//   if (isAdminRoute && !isLoggedIn) {
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
+  // Belum login tapi akses halaman admin → redirect ke login
+  if (isAdminRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-//   // Bukan admin tapi akses halaman users → redirect ke dashboard
-//   if (isUsersRoute && role !== "ADMIN") {
-//     return NextResponse.redirect(new URL("/admin", req.url));
-//   }
+  // Bukan admin tapi akses halaman users → redirect ke dashboard
+  if (isUsersRoute && role !== "ADMIN") {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/admin/:path*", "/login"],
-// };
+export const config = {
+  matcher: ["/admin/:path*", "/login"],
+};
